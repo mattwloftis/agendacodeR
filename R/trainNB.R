187929011dd2f9f08e02906a566ebac85c0fc03d
Matt W. Loftis <mattwloftis@gmail.com>
2016-10-10 10:25:02 +0200

    Added smoothing options and documentation for them.

--- R/trainNB.R
+++ R/trainNB.R
@@ -1,8 +1,11 @@
 #' Train Naive Bayes
 #'
 #' Trains multiclass Naive Bayes classifier
-#' @param coding The numeric vector of codings
+#' @param coding Numeric vector of training document codings
 #' @param train_matrix A \code{quanteda} document-feature matrix with the number of rows equal to the length of \code{coding}
+#' @param smoothing Type of Laplacian smoothing for term priors. See 'Details'.
+#' @param alpha Smoothing hyperparameter for 'parameterized' smoothing
+#' @param beta Smoothing hyperparameter for 'parameterized' smoothing
 #'
 #' @return A list with the elements
 #' \item{w_0c}{Constant portion of NB classification probabilities.}
@@ -10,7 +13,37 @@
 #' \item{nc}{Frequency of each category in training documents (named numeric vector)}
 #' \item{theta_c}{Unsmoothed prior class probabilities (named numeric vector)}
 #'
+#' @details
+#' Smoothing method defaults to 'normalized' using the system advocated by
+#' Frank and Bouckaert (2006) for per-class word vector normalization.
+#'
+#' Using 'simple' will employ a simple version of Laplacian smoothing described
+#' in Metsis et al. (2006). Prior probability of term appearance, given a class,
+#' is just frequency of term in class plus 1 over count of documents in class
+#' plus 2.
+#'
+#' Using 'parameterized' will use a version of smoothing mentioned in O'Neil &
+#' Schutt (2013) for multiclass Naive bayes. Prior prob. of term appearance,
+#' given a class, is frequency of term in class plus alpha minus 1 over count
+#' of documents in class plus alpha plus beta minus 2.
+#'
+#' Using 'none' is inadvisable. In this case, prior prob. of term appearance,
+#' given a class, is frequency of term in class over count of documents in class.
+#' This will likely generate zero priors, which is a problem.
+#'
 #' @author Matt W. Loftis
+#'
+#' @references
+#' Frank, E. and Bouckaert, R.R. (2006) Naive Bayes for Text Classification with
+#' Unbalanced Classes. s, Knowledge Discovery in Databases: PKDD, 503-510.
+#'
+#' Metsis, V. Androutsopoulos, I. and Paliouras, G. (2006) Spam Filtering with
+#' Naive Bayes -- Which Naive Bayes? CEAS 2006 - Third Conference on Email and
+#' Anti-Spam, July 27-28, 2006, Mountain View, California USA.
+#'
+#' O'Neil, C. and Schutt, R. (2013) Doing Data Science: Straight Talk from the
+#' Frontline. O'Reilly.
+#'
 #' @examples
 #' ## Load data and create document-feature matrices
 #' train_corpus <- quanteda::corpus(x = training_agendas$text)
@@ -39,7 +72,8 @@
 #' @export
 
 
-trainNB <- function(coding, train_matrix){ ##TRAINING CLASSIFIER
+trainNB <- function(coding, train_matrix, smoothing = c("normalized",
+                "simple", "parameterized", "none"), alpha = 2, beta = 10) { ##TRAINING CLASSIFIER
   ##Error catching and warnings
   if (length(coding) != nrow(train_matrix)) {
     stop('Number of codings does not equal number of documents in training document-feature matrix')
@@ -77,11 +111,28 @@ trainNB <- function(coding, train_matrix){ ##TRAINING CLASSIFIER
 
   ##Setting prior prob of words in docs by categories in the training set ################
   #NB: Priors set wrt total terms use in category proposed in Frank and Bouckaert 2006
-  j <- ncol(train_matrix) #total number of terms (1 x 1)
-  n_notj_c <- matrix(rep(apply(njc, 1, sum), ncol(njc)),
-                     ncol = ncol(njc), nrow = (nrow(njc))) - njc #freq of all j in c minus freq of j in c (c x j)
-  theta_jc <- (1 + njc) / (j + n_notj_c)
-  theta_jc[theta_jc > 1] <- .9999999999999999 #ceiling on odds greater than one
+  if (smoothing == "normalized"){
+    j <- ncol(train_matrix) #total number of terms (1 x 1)
+    n_notj_c <- matrix(rep(apply(njc, 1, sum), ncol(njc)),
+                       ncol = ncol(njc), nrow = (nrow(njc))) - njc #freq of all j in c minus freq of j in c (c x j)
+    theta_jc <- (1 + njc) / (j + n_notj_c)
+    theta_jc[theta_jc > 1] <- .9999999999999999 #ceiling on odds greater than one
+  }
+
+  #NB: Laplacian priors proposed in Metsis et al 2006
+  if (smoothing == "simple"){
+    theta_jc <- (njc + 1) / (nc + 10)
+  }
+
+  #NB: With smoothing hyperparameters proposed in O'Neil & Schutt 2013
+  if (smoothing == "parameterized"){
+    theta_jc <- (njc + alpha - 1) / (nc + alpha + beta - 2)
+  }
+
+  #NB: This is inadvisable. In fact, don't do it. You'll almost certainly get zero priors.
+  if (smoothing == "none"){
+    theta_jc <- (njc) / (nc)
+  }
 
   ########################################################################################
   ## Main training steps
